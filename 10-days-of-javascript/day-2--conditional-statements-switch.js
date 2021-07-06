754691ceda6622f417a29ba1fc0b893fff9fe405
Enes Baspinar <enesbaspinar07@gmail.com>
2021-07-06 13:42:15 +0300

    Add solution for Day 2: Conditional Statements Switch

---
+++ 10-days-of-javascript/day-2--conditional-statements-switch.js
@@ -0,0 +1,36 @@
+function getLetter(s) {
+  let letter;
+
+  const firstLetter = s.toLowerCase().charAt(0);
+
+  switch (firstLetter) {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u": {
+      return "A";
+    }
+    case "b":
+    case "c":
+    case "d":
+    case "f":
+    case "g": {
+      return "B";
+    }
+    case "h":
+    case "j":
+    case "k":
+    case "l":
+    case "m": {
+      return "C";
+    }
+    default: {
+      return "D";
+    }
+  }
+}
+
+console.log(
+  getLetter('Enes')
+);
